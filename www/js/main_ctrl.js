f02749382a392c454961ef30596cbbe98a41fa75
Kayla Jones <kaylalyne424@gmail.com>
2014-10-16 14:50:22 -0500

    might have ios gyro set up...

--- www/js/main_ctrl.js
+++ www/js/main_ctrl.js
@@ -3,13 +3,30 @@
   angular.module('kj-ball')
   .controller('MainCtrl', ['$scope', function($scope){
     $scope.hello = 'Hello';
-//get device size
-    $scope.width = document.documentElement.clientWidth;
-    $scope.height = document.documentElement.clientHeight;
+    //get device size
+    $scope.width = document.documentElement.clientWidth * 1;
+    $scope.height = document.documentElement.clientHeight * 1;
+    $scope.height = $scope.height - 40;
+    $scope.height = $scope.width - 40;
     // this gets the pixel density---probably wont use for the time being.
-    $scope.availwidth = window.screen.availWidth;
+    $scope.availWidth = window.screen.availWidth;
     $scope.availHeight = window.screen.availHeight;
-//place the ball randomly on the page
-
+    //place the ball randomly on the page
+    $scope.y = Math.floor(Math.random()*($scope.height - 40 + 1) + 40);
+    $scope.x = Math.floor(Math.random()*($scope.width - 40 + 1) + 40);
+    $scope.yHole = Math.floor(Math.random()*($scope.height - 50 + 1) + 50);
+    $scope.xHole = Math.floor(Math.random()*($scope.width - 50 + 1) + 50);
+    //Math.floor(Math.random() * (max - min + 1) + min);
+  function success(orientation){
+    console.log('alpha:'+ orientation.alpha + ', beta' + orientation.beta +', gamma' + orientation.gamma);
+    $scope.orientation = orientation;
+    $scope.$digest();
+  }
+  function error(err){
+    console.log('error', err);
+  }
+  $scope.start = function(){
+    navigator.gyroscope.watchGyroscope(success, error, {frequency:100});
+  };
   }]);
 })();
