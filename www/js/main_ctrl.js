471a5069a9284ee0d892c737c020467fca00961f
Kayla Jones <kaylalyne424@gmail.com>
2014-10-17 15:00:44 -0500

    playing with a new way of using canvas

--- www/js/main_ctrl.js
+++ www/js/main_ctrl.js
@@ -1,7 +1,9 @@
 (function(){
   'use strict';
   angular.module('kj-ball')
-  .controller('MainCtrl', ['$scope', function($scope){
+  .controller('MainCtrl', ['$scope', '$interval', function($scope, $interval){
+    var canvas,
+        context;
 
     //get device size
     $scope.trueWidth = document.documentElement.clientWidth * 1;
@@ -9,10 +11,39 @@
     $scope.height = $scope.trueHeight - 107;
     $scope.width = $scope.trueWidth - 20;
 
-    //$scope.height = $scope.width - 40;
-    // this gets the pixel density---probably wont use for the time being.
-    $scope.availWidth = window.screen.availWidth;
-    $scope.availHeight = window.screen.availHeight;
+    //canvas
+    function init(){
+      canvas = document.getElementById('gameboard');
+      context = canvas.getContext('2d');
+
+      window.addEventListener('resize', resizeCanvas, true);
+      window.addEventListener('orientationchange', resizeCanvas, true);
+      resizeCanvas();
+
+      context.fillStyle = #289eb5;
+      context.fillRect(0, 0, $scope.width, $scope.height);
+      // CIRCLE----------------------
+      //centerX = $scope.xHole,
+      //centerY = $scope.yHole,
+      //radius = 40;
+      //context.beginPath();
+      //context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+      //context.fillStyle = 'black';
+      //context.fill();
+      //context.lineWidth = 2;
+      //context.strokeStyle = 'red';
+      //context.stroke();
+      //---------------------------------
+
+
+    }
+
+
+    function resizeCanvas(){
+      canvas.height = window.innerHeight;
+      canvas.width = window.innerWidth;
+    }
+
     //place the ball randomly on the page
     $scope.y = Math.floor(Math.random()*($scope.height - 1 + 1) + 1);
     $scope.x = Math.floor(Math.random()*($scope.width - 1 + 1) + 1);
@@ -34,33 +65,6 @@
       $scope.$digest();
     });
 
-    //canvas
-    function draw(){
-      var canvas = document.getElementById('gameboard'),
-          context = canvas.getContext('2d'),
-      //canvasContext.fillRect(50, 25, 150, 100);
-      myGradient = context.createLinearGradient(0, 0, $scope.width, $scope.height);
-      myGradient.addColorStop(0, '#674a96');
-      myGradient.addColorStop(1, '#289eb5');
-      context.fillStyle = myGradient;
-      context.fillRect(0, 0, $scope.width, $scope.height);
-
-      var centerX = $scope.xHole,
-          centerY = $scope.yHole,
-          radius = 40;
-      context.beginPath();
-      context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-      context.fillStyle = 'black';
-      context.fill();
-      context.lineWidth = 2;
-      context.strokeStyle = 'black';
-      context.stroke();
-
-      //context.arc(x, y, radius, 0, Math.PI * 2);
-      //context.fill();
-      //context.stroke();
-    }
-    draw();
-
+    init();
   }]);
 })();
