5f32aabb676da1a54eccceb3b6e51a558b95ddf3
Kayla Jones <kaylalyne424@gmail.com>
2014-10-16 22:12:07 -0500

    ball rolls and stays on the screen

--- www/js/main_ctrl.js
+++ www/js/main_ctrl.js
@@ -2,18 +2,19 @@
   'use strict';
   angular.module('kj-ball')
   .controller('MainCtrl', ['$scope', function($scope){
-    $scope.hello = 'Hello';
+
     //get device size
     $scope.width = document.documentElement.clientWidth * 1;
     $scope.height = document.documentElement.clientHeight * 1;
-    $scope.height = $scope.height - 40;
-    $scope.height = $scope.width - 40;
+    $scope.height = $scope.height - 107;
+    $scope.width = $scope.width - 20;
+    //$scope.height = $scope.width - 40;
     // this gets the pixel density---probably wont use for the time being.
     $scope.availWidth = window.screen.availWidth;
     $scope.availHeight = window.screen.availHeight;
     //place the ball randomly on the page
-    $scope.y = Math.floor(Math.random()*($scope.height - 40 + 1) + 40);
-    $scope.x = Math.floor(Math.random()*($scope.width - 40 + 1) + 40);
+    $scope.y = Math.floor(Math.random()*($scope.height - 1 + 1) + 1);
+    $scope.x = Math.floor(Math.random()*($scope.width - 1 + 1) + 1);
     $scope.yHole = Math.floor(Math.random()*($scope.height - 50 + 1) + 50);
     $scope.xHole = Math.floor(Math.random()*($scope.width - 50 + 1) + 50);
     //Math.floor(Math.random() * (max - min + 1) + min);
@@ -31,9 +32,18 @@
       navigator.gyroscope.watchGyroscope(success, error, {frequency:100});
     };*/
     window.addEventListener('deviceorientation', function(data){
-      $scope.data = data;
+      var yOffset = data.beta / 5,
+          xOffset = data.gamma / 10;
+
+      if (($scope.y + yOffset) <= $scope.height && ($scope.y + yOffset) >= 0){
+        $scope.y += yOffset;
+      }
+      if (($scope.x + xOffset) <= $scope.width && ($scope.x + xOffset) >= 0){
+        $scope.x += xOffset;
+      }
       $scope.$digest();
     });
+
 
   }]);
 })();
